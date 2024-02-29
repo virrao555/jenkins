045708053f84ce208b0af50146c96072bc13bde7
virrao555 <virrao555@gmail.com>
2024-02-29 10:14:46 +0530

    second commit

--- Abc.java
+++ Abc.java
@@ -3,10 +3,9 @@ public class Abc
 	public static void main(String args[])
 	{
 
-
-		
 		System.out.println("HELLO From GITHUB");
 		System.out.println("But this program is compiled by Jenkins");
 		System.out.println("PUSH");
+		System.out.println("Made Changes");
 	}
 }
